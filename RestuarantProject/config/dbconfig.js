ed42ec45acbe0532853b84462d19eee64f8380a6
Baburao126 <97587177+Baburao126@users.noreply.github.com>
2025-01-29 17:40:40 +0530

    Delete RestuarantProject directory

--- RestuarantProject/config/dbconfig.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('Restuarant', 'root', 'Password@2024', {
-    host: 'localhost',
-    dialect: 'mysql' 
-});
-
-module.exports = sequelize;
--- RestuarantProject/routes/confirmOrdersRoutes.js
+++
@@ -1,9 +0,0 @@
-const { addItems  }=require('../controllers/confirmOrdersControllers');
-
-const express=require('express');
-
-const routes=express.Router();
-
-routes.post('/addItemstoorders',addItems);
-
-module.exports=routes;
